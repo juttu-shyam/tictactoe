5766ab8002b8cdda004fc276d84b3372f4454d1a
Shyam Sundar <juttushyam@gmail.com>
2025-06-16 17:00:37 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,43 +1,55 @@
 import random
-def inp():
-    c=int(input("1: "))
-    d=int(input("2: "))
-    if a[c-1][d-1]==" ":
-        return c-1,d-1
+import time
+def get_inp():
+    r=int(input("enter the row: "))
+    c=int(input("enter the column: "))
+    if r>3 or c>3:
+        print('you have enter a valid input')
+        time.sleep(1)
+        print('enter a value within 1 to 3')
+        return get_inp()
     else:
-        print("already taken,try again")
-        return inp()
-    #implement out of range
+        if a[r-1][c-1]=="_":
+            return r-1,c-1
+        else:
+            print("already taken,try again")
+            return get_inp()
+    #ValueError
 
-def com():
+def gen_inp():
     e=random.choice([0, 1, 2])
     f=random.choice([0, 1, 2])
-    #print(e,f)
-    if a[e][f]==" ":
+    if a[e][f]=="_":
         return e,f
     else:
-        return com()
+        return gen_inp()
 
-a=[[" "," "," "],[" "," "," "],[" "," "," "]]
-"""n1=random.choice([0, 1, 2])
-n2=random.choice([0, 1, 2])
-print(n1,n2)"""
+def display(x):
+    for i in x:
+        for j in i:
+            print(j,end='|')
+        print()
+
+a=[["_","_","_"],
+   ["_","_","_"],
+   ["_","_","_"]]
 while True:
-    num1,num2=inp()
+    num1,num2=get_inp()
     a[num1][num2]="x"
-    print(a[0],a[1],a[2],sep="\n")
+    display(a)
     if (a[0][0]==a[0][1]==a[0][2]=="x" or a[1][0]==a[1][1]==a[1][2]=="x" or a[2][0]==a[2][1]==a[2][2]=="x" or a[0][0]==a[1][0]==a[2][0]=="x" or a[0][1]==a[1][1]==a[2][1]=="x" or a[0][2]==a[1][2]==a[2][2]=="x" or a[0][0]==a[1][1]==a[2][2]=="x" or a[0][2]==a[1][1]==a[2][0]=="x"):
-        print("\n\nwin")
+        print("\n\nyou have won the match")
         break
-
-    if a[0][0]!=" " and a[0][1]!=" " and a[0][2]!=" " and a[1][0]!=" " and a[1][1]!=" " and a[1][2]!=" " and a[2][0]!=" " and a[2][1]!=" " and a[2][2]!=" ":
-        print("\n\ndraw")
+    
+    if a[0][0]!="_" and a[0][1]!="_" and a[0][2]!="_" and a[1][0]!="_" and a[1][1]!="_" and a[1][2]!="_" and a[2][0]!="_" and a[2][1]!="_" and a[2][2]!="_":
+        print("\n\nlets play again, it an draw")
         break
-    n1,n2=com()
+    n1,n2=gen_inp()
     a[n1][n2]="o"
-    print("my choice in ",n1+1,n2+1)
-    print(a[0],a[1],a[2],sep="\n")
+    time.sleep(2)
+    print("choice made by computer",n1+1,n2+1)
+    time.sleep(2)
+    display(a)
     if (a[0][0]==a[0][1]==a[0][2]=="o" or a[1][0]==a[1][1]==a[1][2]=="o" or a[2][0]==a[2][1]==a[2][2]=="o" or a[0][0]==a[1][0]==a[2][0]=="o" or a[0][1]==a[1][1]==a[2][1]=="o" or a[0][2]==a[1][2]==a[2][2]=="o" or a[0][0]==a[1][1]==a[2][2]=="o" or a[0][2]==a[1][1]==a[2][0]=="o"):
-        print("\n\nloss")
+        print("\n\nyou have lost the match")
         break
-
